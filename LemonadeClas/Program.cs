b46d49c87630f4cbd7a25581e03a482a0b712da9
Your Name <you@email.com>
2016-05-21 17:03:32 -0400

    Trying to refactor.

--- LemonadeClas/Program.cs
+++ LemonadeClas/Program.cs
@@ -8,6 +8,12 @@ namespace LemonadeClas
 {
     class Program
     {
+        public static void AnyKey()
+        {
+            Console.WriteLine("Press any key to continue.");
+            Console.ReadKey();
+            Console.Clear();
+        }
         public static void Greeting()
         {
             Console.ForegroundColor = ConsoleColor.Yellow;
@@ -27,28 +33,24 @@ namespace LemonadeClas
         }
         static void Main(string[] args)
         {
+            //some initial values
             int dayCount = 1;
             int timeIncrement = 0;
             int custCount = 0;
             int soldCount = 0;
+            string yorn = "y";//do you want to play again (begin with y to play at all)
             Greeting();
-
             Console.WriteLine("What is your name?");
             string playerName = Console.ReadLine();
             Player player = new Player(playerName, 0);
             Lemonstuff currentLemonade = new Lemonstuff(0);
-            //currentLemonade.LemonStatus(); //TESTING
             Console.WriteLine("The cost of a pitcher of lemonade is $3 - it will come from your profits!");
-            // Console.WriteLine("Let's make some lemonade to start the game. \nWe'll subtract the cost at the end of the day.");
-            while (dayCount < 3)
+            while (yorn == "y")//this is not working
             {
                 currentLemonade.MakeLemonade();
                 player.cash = player.cash - 3;
-                Console.WriteLine("Press any key to continue.");
-                Console.ReadKey();
-                Console.Clear();
+                AnyKey();
                 WeatherTime today = new WeatherTime(0);
-                //Console.WriteLine(currentLemonade.makeLemonadeCount); TESTING
                 while (timeIncrement < 13)
                 {
                     timeIncrement = timeIncrement + 1;
@@ -59,71 +61,29 @@ namespace LemonadeClas
                         today.WeatherCall();
                         currentLemonade.LemonStatus();
                     }
-                    else if (timeIncrement == 13)
-                    {
-                        //currentLemonade.ReturnLemonadeCount();
-                        player.cash = player.cash - ((currentLemonade.ReturnLemonadeCount() - 1) * 3);
-                        Console.ForegroundColor = ConsoleColor.Yellow;
-                        Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
-                        Console.WriteLine("            Day {0} is over!               ", dayCount);
-                        Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
-                        dayCount = dayCount + 1;
-                        Console.Write(playerName);
-                        Console.WriteLine(" sold {0} cups of lemonade!\nBut the leftover lemonade can't be saved. Yuck.", soldCount);
-                        Console.WriteLine("After removing $3 ingredients per pitcher, your total cash is ${0}!", player.cash);
-                        Console.ResetColor();
-                        timeIncrement = 0;
-                        Console.WriteLine("Press any key to continue.");
-                        Console.ReadKey();
-                        int goodAnswer = 0;
-                        do
-                        {
-                            Console.WriteLine("Ready for a new day? (Y/N)");
-                            string yorn = Console.ReadLine().ToLower();
-                            if (yorn == "n")
-                            {
-                                Console.WriteLine("Hope you had fun!");
-                                Console.WriteLine("Press any key to exit.");
-                                Console.ReadKey();
-                                Environment.Exit(0);
-                            }
-                            else if (yorn == "y")
-                            {
-                                continue;
-                            }
-                            else
-                            {
-                                Console.WriteLine("What? Please just answer Y or N.");
-                            }
-                        } while (goodAnswer == 0);
-                        Console.Clear();
-                    }
+                   
                     else {
                         custCount = custCount + 1;
                         Customer Cust = new Customer();
                         Cust.SetThirst();
                         if (today.weatherNow != "sunny" && Cust.CustomerCold != "Warm" && today.tempNow == "cold")
                         {
-                            //Console.WriteLine("Today the weather is " + today.weatherNow);
                             Console.ForegroundColor = ConsoleColor.Cyan;
                             Console.WriteLine("No one comes for awhile because the weather isn't nice enough.");
                             Console.ResetColor();
                             Console.WriteLine("You wait, to see if the weather improves.");
                             timeIncrement = timeIncrement + 1;
                             custCount = custCount - 1;
-                            today.WeatherCall();
-                            Console.WriteLine("Press any key to continue.");
-                            Console.ReadKey();
+                            today.weatherNow = "sunny";
+                            today.tempNow = "warm";
+                            AnyKey();
                         }
-                        else {
+                        else if {
                             Console.ForegroundColor = ConsoleColor.Yellow;
                             Console.WriteLine("Right now the weather is " + today.weatherNow + ".");
                             Console.ResetColor();
                             Console.WriteLine("\nHere comes a new customer!");
                             Console.WriteLine("This is your customer #{0} of the day.", custCount);
-                            //Customer Cust = new Customer();
-                            //Console.ReadKey();
-                            //Cust.SetThirst();
                             Console.WriteLine("How much do you want to charge for lemonade?");
                             double price = double.Parse(Console.ReadLine());
                             if (price > 5)
@@ -132,8 +92,7 @@ namespace LemonadeClas
                                 Cust.CustomerThirst = "Not Happy";
                                 Console.WriteLine("The customer leaves in indignation.");
                                 custCount = custCount + 1;
-                                Console.WriteLine("Press any key to continue.");
-                                Console.ReadKey();
+                                AnyKey();
                             }
                             else
                             {
@@ -150,18 +109,16 @@ namespace LemonadeClas
                                 }
                                 Console.Write(Cust.CustomerThirst + ". ");
                                 Console.ResetColor();
-                                if (Cust.CustomerThirst == "Not Happy" && price >= 2)
+                                if (Cust.CustomerThirst == "Not Happy" && price > 2)
                                 {
                                     Console.WriteLine("You try to sell the lemonade, \nbut the customer doesn't want to pay " + price + "!");
                                     Console.WriteLine("Better luck next time?");
-                                    Console.WriteLine("Press any key to continue.");
-                                    Console.ReadKey();
-                                    Console.Clear();
+                                    AnyKey();
                                 }
-                                else if (Cust.CustomerThirst == "Not Happy" && price == 1)
+                                else if (Cust.CustomerThirst == "Not Happy" && price <= 2)
                                 {
                                     Console.ForegroundColor = ConsoleColor.Green;
-                                    Console.Write("But...the customer changes his mind\n");
+                                    Console.Write("But...the customer decided to buy some\n");
                                     Console.ResetColor();
                                     Console.WriteLine("because your lemonade is so reasonably priced!");
                                     currentLemonade.lemonade = currentLemonade.lemonade - 1;
@@ -170,9 +127,7 @@ namespace LemonadeClas
                                     Console.WriteLine("You sell a cup of lemonade to the thirsty customer.");
                                     currentLemonade.LemonStatus();
                                     Console.WriteLine("You now have ${0}!", player.cash);
-                                    Console.WriteLine("Press any key to continue.");
-                                    Console.ReadKey();
-                                    Console.Clear();
+                                    AnyKey();
                                 }
                                 else
                                 {
@@ -182,16 +137,51 @@ namespace LemonadeClas
                                     Console.WriteLine("You sell a cup of lemonade to the thirsty customer.");
                                     currentLemonade.LemonStatus();
                                     Console.WriteLine("You now have ${0}!", player.cash);
-                                    Console.WriteLine("Press any key to continue.");
-                                    Console.ReadKey();
-                                    Console.Clear();
+                                    AnyKey();
                                 }
+                             else if (timeIncrement == 13)
+                            {
+                                player.cash = player.cash - ((currentLemonade.ReturnLemonadeCount() - 1) * 3);
+                                Console.ForegroundColor = ConsoleColor.Yellow;
+                                Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+                                Console.WriteLine("            Day {0} is over!               ", dayCount);
+                                Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+                                dayCount = dayCount + 1;
+                                Console.Write(playerName);
+                                Console.WriteLine(" sold {0} cups of lemonade!\nBut the leftover lemonade can't be saved. Yuck.", soldCount);
+                                Console.WriteLine("After removing $3 ingredients per pitcher, your total cash is ${0}!", player.cash);
+                                Console.ResetColor();
+                                AnyKey();
+                                int goodAnswer = 0;
+                                do
+                                {
+                                    Console.WriteLine("Ready for a new day? (Y/N)");
+                                    yorn = Console.ReadLine().ToLower();
+                                    if (yorn == "n")
+                                    {
+                                        Console.WriteLine("Hope you had fun!");
+                                        Console.WriteLine("Press any key to exit."); //this works
+                                        Console.ReadKey();
+                                        Environment.Exit(0);
+                                    }
+                                    else if (yorn == "y")
+                                    {
+                                        timeIncrement = 0;
+                                        Console.WriteLine("You answered Yes but I failed anyway.");//this does not work.
+                                    }
+                                    else
+                                    {
+                                        Console.WriteLine("What? Please just answer Y or N."); //this works
+                                    }
+                                } while (goodAnswer == 0);
+                                Console.Clear();
+                            }
                             }
                         }
                     }
-                }
-                           // Console.WriteLine("The end?"); //test to see where this shows up
-            }
+                } 
+            } 
+          Console.WriteLine("The end?"); //test to see where this shows up
         }
     }
 }
